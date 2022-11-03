182e6df063c6bb8381b9b7b621935e47e41b6281
0203395 <112115286+0203395@users.noreply.github.com>
2022-11-03 10:32:39 -0400

    Update HelloTest1.java
    I added comments to lines 2 & 8.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,10 +1,11 @@
 /*
  * Updated on 10/8/21 by DXE
+ * Updated on 11/3/22 by TH
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
-    /*
+    /* This is my first Pull Request
      * First Pull Request
      */
     public static void main(String[] args) {
